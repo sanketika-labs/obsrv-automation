e1498e8adcdfc175fa0a7b049953e3e7a7152768
divyagovindaiah24-CTS <divyagovindaiah24@gmail.com>
2026-01-28 19:30:05 +0530

    Deployment of services.

--- helmcharts/kitchen/install.sh
+++ helmcharts/kitchen/install.sh
@@ -117,7 +117,7 @@ obsrvtools)
     ;;
 additional)
     cp -rf ../obsrv additional
-    cp -rf ../services/{spark,system-rules-ingestor,secor,druid-exporter,postgresql-exporter,postgresql-backup,kong-ingress-routes,velero,volume-autoscaler,masterdata-indexer-cron} additional/charts/
+    cp -rf ../services/{postgresql-backup,kong-ingress-routes,volume-autoscaler,masterdata-indexer-cron} additional/charts/
     # copy cloud specific helm charts
     case $cloud_env in
     "aws")
@@ -145,7 +145,7 @@ core-setup)
     ;;
 all)
     bash $0 migrations ${@: 2}
-    bash $0 monitoring ${@: 2}
+    # bash $0 monitoring ${@: 2}
     bash $0 oauth ${@: 2}
     bash $0 coreinfra ${@: 2}
     bash $0 obsrvapis ${@: 2}
